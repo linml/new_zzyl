c3512c36098fd3c8c5e60963a87010497ff2c59a
Your Name <you@example.com>
2019-07-25 13:37:56 +0800

    修改download/download.php文件

--- download/download.php
+++ download/download.php
@@ -235,13 +235,13 @@ if (!defined('zzyl')) {
             var u = navigator.userAgent;
             var isAndroid = u.indexOf('Android') > -1 || u.indexOf('Adr') > -1 || u.indexOf('android') > -1; //android终端
             if (isAndroid) {
-                //location.href = "<?php echo $gameConfig['android_packet_address'] ? $gameConfig['android_packet_address'] : 'anzhuo.apk';?>"
+                location.href = "<?php echo $gameConfig['android_packet_address'] ? $gameConfig['android_packet_address'] : 'anzhuo.apk';?>"
             } else {
                 var isiOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); //ios终端
                 if (isiOS) {
-                    //location.href = "<?php echo $gameConfig['apple_packet_address'];?>";
+                    location.href = "<?php echo $gameConfig['apple_packet_address'];?>";
                 } else {
-                    //location.href = "<?php echo $gameConfig['android_packet_address'] ? $gameConfig['android_packet_address'] : 'anzhuo.apk';?>"
+                    location.href = "<?php echo $gameConfig['android_packet_address'] ? $gameConfig['android_packet_address'] : 'anzhuo.apk';?>"
                 }
             }
 
